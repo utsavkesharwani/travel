db379e4ee085080c5b9e52c4b198703e4bcff569
Utsav Kesharwani <utsav.kesharwani@gmail.com>
2016-07-05 12:28:44 +0530

    integrated email send

--- lib/user_notifier.rb
+++ lib/user_notifier.rb
@@ -1,4 +1,14 @@
+require 'net/smtp'
+require_relative '../constants'
+
 class UserNotifier
+
+  $message = <<EOF
+From: SENDER <#{KEYS['mail_credentials']['user_name']}>
+To: RECEIVER <replace_receiver>
+Subject: Time to book an uber!
+Time to book an uber!
+EOF
 
   attr_accessor :user_email
 
@@ -7,6 +17,10 @@ class UserNotifier
   end
 
   def inform_user_to_book_uber
-    # To-Do: Add method definition
+    smtp = Net::SMTP.new KEYS['mail_credentials']['address'], KEYS['mail_credentials']['port']
+    smtp.enable_starttls
+    smtp.start(KEYS['mail_credentials']['domain'], KEYS['mail_credentials']['user_name'], KEYS['mail_credentials']['password'], :login)
+    smtp.send_message $message.gsub('replace_receiver', self.user_email), KEYS['mail_credentials']['user_name'], self.user_email
+    smtp.finish
   end
 end
